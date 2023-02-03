0383d321e01db30279c28582b118633a69fcefb9
James Sentinella <james@sentinella.me>
2023-02-03 14:33:11 -0800

    Changing default zero conf and control ports.

--- src/defaults.rs
+++ src/defaults.rs
@@ -1,7 +1,7 @@
 use crate::types;
 
 pub const DEFAULT_SIGHTING_PERIOD: u32 = 5 * 60;
-pub const DEFAULT_ZERO_CONF_PORT: u16 = 45610;
-pub const DEFAULT_CONTROL_PORT: u16 = 45620;
+pub const DEFAULT_ZERO_CONF_PORT: u16 = 4488;
+pub const DEFAULT_CONTROL_PORT: u16 = 4498;
 pub const DEFAULT_CHIP_TYPE: &str = types::TYPE_CHIP_DEC;
 pub const DEFAULT_READ_WINDOW: u8 = 20;
