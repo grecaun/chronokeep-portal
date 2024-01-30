1c970730832c374244be71e99723eb822c9ca7c2
James Sentinella <james@sentinella.me>
2024-01-30 00:32:22 -0800

    Modified deploy script some more. Changed cross compile into two scripts, one to compile and one to set up the github action environment.

--- cross_compile.sh
+++ cross_compile.sh
@@ -1,38 +1,6 @@
 #!/bin/bash
 
 echo "---------------------------------------------------------------------------------"
-echo Adding architectures.
-echo "---------------------------------------------------------------------------------"
-sudo dpkg --add-architecture arm64
-sudo dpkg --add-architecture armhf
-echo Setting up apt sources and updating current apt packages.
-echo "---------------------------------------------------------------------------------"
-echo deb [arch=armhf,arm64] http://ports.ubuntu.com/ jammy main restricted | sudo tee /etc/apt/sources.list.d/arm-cross-compile-sources.list
-echo deb [arch=armhf,arm64] http://ports.ubuntu.com/ jammy-updates main restricted | sudo tee -a /etc/apt/sources.list.d/arm-cross-compile-sources.list
-echo deb [arch=armhf,arm64] http://ports.ubuntu.com/ jammy universe | sudo tee -a /etc/apt/sources.list.d/arm-cross-compile-sources.list
-echo deb [arch=armhf,arm64] http://ports.ubuntu.com/ jammy-updates universe | sudo tee -a /etc/apt/sources.list.d/arm-cross-compile-sources.list
-echo deb [arch=armhf,arm64] http://ports.ubuntu.com/ jammy multiverse | sudo tee -a /etc/apt/sources.list.d/arm-cross-compile-sources.list
-echo deb [arch=armhf,arm64] http://ports.ubuntu.com/ jammy-updates multiverse | sudo tee -a /etc/apt/sources.list.d/arm-cross-compile-sources.list
-echo deb [arch=armhf,arm64] http://ports.ubuntu.com/ jammy-backports main restricted universe multiverse | sudo tee -a /etc/apt/sources.list.d/arm-cross-compile-sources.list
-echo deb [arch=amd64] http://archive.ubuntu.com/ubuntu jammy main restricted universe multiverse | sudo tee /etc/apt/sources.list
-echo deb [arch=amd64] http://archive.ubuntu.com/ubuntu/ jammy-updates main restricted universe multiverse | sudo tee -a /etc/apt/sources.list
-echo deb [arch=amd64] http://security.ubuntu.com/ubuntu/ jammy-security main restricted universe multiverse | sudo tee -a /etc/apt/sources.list
-echo deb [arch=amd64] http://archive.ubuntu.com/ubuntu/ jammy-backports main restricted universe multiverse | sudo tee -a /etc/apt/sources.list
-sudo apt update
-sudo apt upgrade -y
-echo "---------------------------------------------------------------------------------"
-echo Installing required apt packages.
-echo "---------------------------------------------------------------------------------"
-sudo apt install gcc pkg-config libasound2-dev libssl-dev git gcc-arm-linux-gnueabihf libasound2-dev:armhf libasound2-dev:arm64 -y
-echo "---------------------------------------------------------------------------------"
-echo Installing rust.
-echo "---------------------------------------------------------------------------------"
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
-source "$HOME/.cargo/env"
-echo "---------------------------------------------------------------------------------"
-echo Adding rust target
-rustup target add armv7-unknown-linux-gnueabihf
-echo "---------------------------------------------------------------------------------"
 echo Building portal software for host architecture.
 echo "---------------------------------------------------------------------------------"
 cargo build --release
@@ -59,14 +27,5 @@ if [ $? -eq 0 ]; then
 else
     echo "---------------------------------------------------------------------------------"
     echo Unable to cross compile portal software for armv7-unknown-linux-gnueabihf.
-    echo "---------------------------------------------------------------------------------"
-fi;
-if [[ -e version.txt ]]; then
-    echo Copying version text files.
-    echo "---------------------------------------------------------------------------------"
-    cp version.txt target/armv7-unknown-linux-gnueabihf/release/
-    cp version.txt target/release/
-else
-    echo Version file does not exist.
     echo "---------------------------------------------------------------------------------"
 fi;
