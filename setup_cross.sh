0762e11e1386ccb80a0396c7c3e10d00dcd5c56a
James Sentinella <james@sentinella.me>
2024-01-30 00:42:15 -0800

    Changing script back to one file.  Using the new file this time though.

--- setup_cross.sh
+++ setup_cross.sh
@@ -32,3 +32,32 @@ source "$HOME/.cargo/env"
 echo "---------------------------------------------------------------------------------"
 echo Adding rust target
 rustup target add armv7-unknown-linux-gnueabihf
+echo "---------------------------------------------------------------------------------"
+echo Building portal software for host architecture.
+echo "---------------------------------------------------------------------------------"
+cargo build --release
+if [ $? -eq 0 ]; then
+    echo "---------------------------------------------------------------------------------"
+    echo Portal software successfully compiled for host architecture.
+    echo "---------------------------------------------------------------------------------"
+else
+    echo "---------------------------------------------------------------------------------"
+    echo Unable to compile portal software for host architecture.
+    echo "---------------------------------------------------------------------------------"
+fi;
+echo Setting environment variables to cross compile for armv7-unknown-linux-gnueabihf.
+echo "---------------------------------------------------------------------------------"
+export PKG_CONFIG_LIBDIR=/usr/lib/arm-linux-gnueabihf/pkgconfig
+export PKG_CONFIG_ALLOW_CROSS=1
+echo Building software for armv7-unknown-linux-gnueabihf.
+echo "---------------------------------------------------------------------------------"
+cargo build --release --target=armv7-unknown-linux-gnueabihf
+if [ $? -eq 0 ]; then
+    echo "---------------------------------------------------------------------------------"
+    echo Portal software successfully cross compiled for armv7-unknown-linux-gnueabihf.
+    echo "--------------------------------------------------------------------------------"
+else
+    echo "---------------------------------------------------------------------------------"
+    echo Unable to cross compile portal software for armv7-unknown-linux-gnueabihf.
+    echo "---------------------------------------------------------------------------------"
+fi;
