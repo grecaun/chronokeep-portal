3a203a4523211be7da384fb1d610602c3216d7c9
James Sentinella <james@sentinella.me>
2024-02-05 17:55:11 -0800

    Fixed deploy script error.

--- deploy.sh
+++ deploy.sh
@@ -1,25 +1,38 @@
 #!/bin/bash
 
 echo "---------------------------------------------------------------------------------"
-echo Updating current apt packages.
+echo Adding architectures.
 echo "---------------------------------------------------------------------------------"
+sudo dpkg --add-architecture arm64
+sudo dpkg --add-architecture armhf
+echo Setting up apt sources and updating current apt packages.
+echo "---------------------------------------------------------------------------------"
+echo deb [arch=armhf,arm64] http://ports.ubuntu.com/ jammy main restricted | sudo tee /etc/apt/sources.list.d/arm-cross-compile-sources.list
+echo deb [arch=armhf,arm64] http://ports.ubuntu.com/ jammy-updates main restricted | sudo tee -a /etc/apt/sources.list.d/arm-cross-compile-sources.list
+echo deb [arch=armhf,arm64] http://ports.ubuntu.com/ jammy universe | sudo tee -a /etc/apt/sources.list.d/arm-cross-compile-sources.list
+echo deb [arch=armhf,arm64] http://ports.ubuntu.com/ jammy-updates universe | sudo tee -a /etc/apt/sources.list.d/arm-cross-compile-sources.list
+echo deb [arch=armhf,arm64] http://ports.ubuntu.com/ jammy multiverse | sudo tee -a /etc/apt/sources.list.d/arm-cross-compile-sources.list
+echo deb [arch=armhf,arm64] http://ports.ubuntu.com/ jammy-updates multiverse | sudo tee -a /etc/apt/sources.list.d/arm-cross-compile-sources.list
+echo deb [arch=armhf,arm64] http://ports.ubuntu.com/ jammy-backports main restricted universe multiverse | sudo tee -a /etc/apt/sources.list.d/arm-cross-compile-sources.list
+echo deb [arch=amd64] http://archive.ubuntu.com/ubuntu jammy main restricted universe multiverse | sudo tee /etc/apt/sources.list
+echo deb [arch=amd64] http://archive.ubuntu.com/ubuntu/ jammy-updates main restricted universe multiverse | sudo tee -a /etc/apt/sources.list
+echo deb [arch=amd64] http://security.ubuntu.com/ubuntu/ jammy-security main restricted universe multiverse | sudo tee -a /etc/apt/sources.list
+echo deb [arch=amd64] http://archive.ubuntu.com/ubuntu/ jammy-backports main restricted universe multiverse | sudo tee -a /etc/apt/sources.list
 sudo apt update
 sudo apt upgrade -y
 echo "---------------------------------------------------------------------------------"
 echo Installing required apt packages.
 echo "---------------------------------------------------------------------------------"
-sudo apt install gcc pkg-config libasound2-dev libssl-dev git -y
+sudo apt install curl gcc pkg-config libasound2-dev git gcc-arm-linux-gnueabihf libasound2-dev:armhf libasound2-dev:arm64 gcc-aarch64-linux-gnu -y 
 echo "---------------------------------------------------------------------------------"
 echo Installing rust.
 echo "---------------------------------------------------------------------------------"
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 source "$HOME/.cargo/env"
 echo "---------------------------------------------------------------------------------"
-echo Getting release version.
-RELEASE_VERSION=$(git describe --tag | sed s/v// | sed -E s/[\.]/\\\\\./g)
-echo "---------------------------------------------------------------------------------"
-echo Updating version in toml file.
-sed -i -E '0,/^version.*$/{s/^version.*$/version = "${RELEASE_VERSION}"/}' Cargo.toml
+echo Adding rust target
+rustup target add armv7-unknown-linux-gnueabihf
+rustup target add aarch64-unknown-linux-gnu
 echo "---------------------------------------------------------------------------------"
 echo Building portal software for host architecture.
 echo "---------------------------------------------------------------------------------"
@@ -31,6 +44,40 @@ if [ $? -eq 0 ]; then
 else
     echo "---------------------------------------------------------------------------------"
     echo Unable to compile portal software for host architecture.
+    echo "---------------------------------------------------------------------------------"
+    exit 1
+fi;
+echo Setting environment variables to cross compile for armv7-unknown-linux-gnueabihf.
+echo "---------------------------------------------------------------------------------"
+export PKG_CONFIG_LIBDIR=/usr/lib/arm-linux-gnueabihf/pkgconfig
+export PKG_CONFIG_ALLOW_CROSS=1
+echo Building software for armv7-unknown-linux-gnueabihf.
+echo "---------------------------------------------------------------------------------"
+cargo build --release --target=armv7-unknown-linux-gnueabihf
+if [ $? -eq 0 ]; then
+    echo "---------------------------------------------------------------------------------"
+    echo Portal software successfully cross compiled for armv7-unknown-linux-gnueabihf.
+    echo "--------------------------------------------------------------------------------"
+else
+    echo "---------------------------------------------------------------------------------"
+    echo Unable to cross compile portal software for armv7-unknown-linux-gnueabihf.
+    echo "---------------------------------------------------------------------------------"
+    exit 1
+fi;
+echo Setting environment variables to cross compile for aarch64-unknown-linux-gnu.
+echo "---------------------------------------------------------------------------------"
+export PKG_CONFIG_LIBDIR=/usr/lib/aarch64-linux-gnu/pkgconfig
+export PKG_CONFIG_ALLOW_CROSS=1
+echo Building software for aarch64-unknown-linux-gnu.
+echo "---------------------------------------------------------------------------------"
+cargo build --release --target=aarch64-unknown-linux-gnu
+if [ $? -eq 0 ]; then
+    echo "---------------------------------------------------------------------------------"
+    echo Portal software successfully cross compiled for aarch64-unknown-linux-gnu.
+    echo "--------------------------------------------------------------------------------"
+else
+    echo "---------------------------------------------------------------------------------"
+    echo Unable to cross compile portal software for aarch64-unknown-linux-gnu.
     echo "---------------------------------------------------------------------------------"
     exit 1
 fi;
