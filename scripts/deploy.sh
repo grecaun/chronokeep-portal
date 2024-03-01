f7f6672507952480437ce893919828edbc9a5dc2
James Sentinella <james@sentinella.me>
2024-02-29 16:36:51 -0800

    Fixed issue with deploy script not automatically setting version.

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -34,6 +34,12 @@ echo Adding rust target
 rustup target add armv7-unknown-linux-gnueabihf
 rustup target add aarch64-unknown-linux-gnu
 echo "---------------------------------------------------------------------------------"
+echo Getting release version.
+RELEASE_VERSION=$(git describe --tag | sed s/v// | sed -E s/[\.]/\\\\\./g | awk -F \- '{print $1}')
+echo "---------------------------------------------------------------------------------"
+echo Updating version in toml file.
+sed -i -E "0,/^version.*$/{s/^version.*$/version = '${RELEASE_VERSION}'/}" Cargo.toml
+echo "---------------------------------------------------------------------------------"
 echo Building portal software for host architecture.
 echo "---------------------------------------------------------------------------------"
 cargo build --release
